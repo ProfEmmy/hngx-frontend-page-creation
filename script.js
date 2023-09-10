db59a6e6991aefbc4ae6af85335558e7c4ebe9f1
ProfEmmy <faithogbole4@gmail.com>
2023-09-10 11:51:07 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const current_time_h2 = document.querySelector("#current_time")
+const current_date_h2 = document.querySelector("#current_date")
+
+const d = new Date()
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+current_date_h2.innerText = days[d.getDay()]
+setInterval(() => {
+    const d = new Date()
+
+    current_time_h2.innerText = `${d.getUTCHours()}:${d.getUTCMinutes()} ${d.getUTCSeconds()} ${d.getUTCMilliseconds()}`
+}, 1)
